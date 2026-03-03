bad316c2b1300b9268307b783387861fd3f7538c
Siva290395 <sivaphudur@gmail.com>
2026-03-03 11:30:43 +0530

    frontend connected to backend

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("http://35.168.90.51:5000")
+    fetch("http://35.168.90.51")
       .then((res) => res.text())
       .then((data) => setMessage(data))
       .catch((err) => console.log(err));
